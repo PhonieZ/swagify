196ce2a6f19cd51f1935b4c1c1e8ee3e027fbf7c
Phoniez <phoniezdev@yahoo.com>
2024-03-14 18:36:54 +0000

    Added Config Reading Class

---
+++ src/phoni/swagify/swagify_config.java
@@ -0,0 +1,42 @@
+package phoni.swagify;
+
+import phoni.swagify.utils.json;
+
+import com.fs.starfarer.api.Global;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+
+
+
+public class swagify_config 
+{   
+    public static final String SEPARATOR = "_";
+    public static final String ALL_PLAYERS = "all_players";
+
+    public static final String CONFIG_FILE = "phoni_swagify_config.json";
+
+    public static Set<String> swag_characters;
+
+
+
+    static 
+    {
+		try 
+        {   
+            JSONObject settings = Global.getSettings().loadJSON(CONFIG_FILE);
+
+			swag_characters = new HashSet<>(json.JSONArray_to_list(settings.optJSONArray("swag_characters")));
+		} 
+        
+
+		catch (IOException | JSONException exception) 
+        {
+			throw new RuntimeException("Failed To Load Swagify Config", exception);
+		}
+	}
+}
