e7486206c273185fc5912ec98c738295fe0e2ce4
Phoniez <phoniezdev@yahoo.com>
2024-03-14 19:21:23 +0000

    Updated swagify_config Formatting

--- src/phoni/swagify/swagify_config.java
+++ src/phoni/swagify/swagify_config.java
@@ -38,8 +38,10 @@ public class swagify_config
 
     public static final String CONFIG_FILE = "phoni_swagify_config.json";
 
+
     public static Set<String> swag_characters;
     public static boolean is_sprite_change_permanent;
+
 
 
 
@@ -49,10 +51,13 @@ public class swagify_config
         {   
             JSONObject settings = Global.getSettings().loadJSON(CONFIG_FILE);
 
+
 			swag_characters = new HashSet<>(json.JSONArray_to_list(settings.optJSONArray("swag_characters")));
             is_sprite_change_permanent = settings.optBoolean("is_sprite_change_permanent", is_sprite_change_permanent);
 		} 
         
+
+
 
 		catch (IOException | JSONException exception) 
         {
